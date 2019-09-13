cb642e6de781b68ba8c9eddfcbdbe86bb5902a86
Björn Guth <scatty@bguth.de>
2019-09-13 11:24:15 +0200

    Change indentation on index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>PHP Store</title>
-  </head>
-  <body>
-    <?php
-    $name = "PHP Store";
-	$credit = 1000;
+	<head>
+		<title>PHP Store</title>
+	</head>
+	<body>
+		<?php
+			$name = "PHP Store";
+			$credit = 1000;
 
-    echo "<h1>Welcome to ".$name."!</h1>";
-	echo "<h2>You have $".$credit." in your wallet.<h2>";
-    ?>
-  </body>
+			echo "<h1>Welcome to ".$name."!</h1>";
+			echo "<h2>You have $".$credit." in your wallet.<h2>";
+		?>
+	</body>
 </html>
